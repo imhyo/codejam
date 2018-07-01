cb7a2205a3121ac9dbadec5d211b150a4f501d59
imhyo <hyojun.im@gmail.com>
2018-07-01 17:46:47 +0900

    Improve lis.py. Added get_lis_size which finds the size of lis (not actual lis) in O(NlogN).

--- lis.py
+++ lis.py
@@ -1,11 +1,40 @@
-# Longest Increasing Sequence
+from bisect import bisect_right
+
+
+# Longest Increasing Sub-sequence
+def get_lis_size(seq):
+    result = [seq[0]]
+    for n in seq:
+        if n > result[-1]:
+            result.append(n)
+        else:
+            index = bisect_right(result, n)
+            if index > 0 and result[index - 1] == n:
+                continue
+            result[index] = n
+
+    return len(result)
+
 
 def get_lis(seq):
-    lis = [1 for _ in range(len(seq))]
+    lis_size = [1 for _ in range(len(seq))]
+    prev = [-1 for _ in range(len(seq))]
 
     for i in range(1, len(seq)):
         for j in range(i):
             if seq[i] > seq[j]:
-                lis[i] = max(lis[i], lis[j] + 1)
+                lis_size[i] = max(lis_size[i], lis_size[j] + 1)
+                prev[i] = j
 
-    return max(lis)
+    max_lis_size = 0
+    index = -1
+    for i in range(len(seq)):
+        if lis_size[i] > max_lis_size:
+            max_lis_size = lis_size[i]
+            index = i
+
+    result = []
+    while index >= 0:
+        result.append(seq[index])
+        index = prev[index]
+    return list(reversed(result))
